77ad537d74e929bf5a11f7d01d61fe229b4db87c
WebsiteTestings <prasaddhend1996@gmail.com>
2021-08-04 16:20:43 +0530

    Dicee Challenge

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
+var randomImage = "/images/dice" + randomNumber1 + ".png";
+console.log(randomImage);
+document.querySelector(".img1").setAttribute("src", randomImage);
+
+//second image
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+var randomImage2 = "/images/dice" + randomNumber2 + ".png";
+console.log(randomImage2);
+document.querySelector(".img2").setAttribute("src", randomImage2);
+
+//Announce Winner
+var winner = document.querySelector("h1");
+console.log(winner);
+if (randomNumber1 > randomNumber2) {
+  winner.innerHTML = "Player 1 Win's";
+} else if (randomNumber1 === randomNumber2) {
+  winner.innerHTML = "Draw !";
+} else if (randomNumber1 < randomNumber2) {
+  winner.innerHTML = "Player 2 Win's";
+}
