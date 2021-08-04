3919906b2b7fe4794bce98461d28af948593e4b8
WebsiteTestings <72221635+WebsiteTestings@users.noreply.github.com>
2021-08-04 16:38:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
-var randomImage = "https://github.com/WebsiteTestings/Dicee_Challenge/blob/main/images/dice1.png" ;
+var randomImage = "https://github.com/WebsiteTestings/Dicee_Challenge/blob/main/images/dice1.PNG" ;
 console.log(randomImage);
 document.querySelector(".img1").setAttribute("src", randomImage);
 
 //second image
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
-var randomImage2 = "https://github.com/WebsiteTestings/Dicee_Challenge/blob/main/images/dice" + randomNumber2 + ".png";
+var randomImage2 = "https://github.com/WebsiteTestings/Dicee_Challenge/blob/main/images/dice" + randomNumber2 + ".PNG";
 console.log(randomImage2);
 document.querySelector(".img2").setAttribute("src", randomImage2);
 
